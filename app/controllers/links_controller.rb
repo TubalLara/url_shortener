bfad43150850cde8cd06a975ca5663caf68d6ddb
Tubal Lara <tublargar@gmail.com>
2015-07-29 09:45:20 +0200

    second iteration done

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,4 +4,22 @@ class LinksController < ApplicationController
 		redirect_to @link.url
 
 	end
+	def new 
+    	@link = Link.new params[:shortlink]
+   	end 
+
+   	def create 
+    @link = Link.new entry_params
+    
+        if @link.save 
+	      redirect_to action: 'new', controller: 'links' 
+	    else 
+	      render 'links/new' 
+	    end 
+  	end
+
+  	private
+  	def entry_params 
+        params.require(:link).permit(:short_url) 
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  get '/' => 'site#index'
+
+  get '/links/new' => 'links#new'
+  
   get '/:shortlink' => 'links#redirect'
+
+  post '/links' => 'links#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
